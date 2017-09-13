6772a7fbf524eb2c3c5f06ee806e92f62dae21eb
Skyler Svendsen <skylersvendsen@Skylers-MacBook-Pro.local>
2017-09-12 22:22:34 -0500

    first commit

---
+++ HwSProject10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  HwSProject10
+//
+//  Created by Skyler Svendsen on 8/30/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
